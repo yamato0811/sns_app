a1e5e15e7ccb9d216015cdb3607524f7b40ad828
yamato0811 <yama8to11@gmail.com>
2023-07-05 15:25:57 +0000

    2-10

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,31 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
-    render :index
+    render 'posts/index'
   end
+
   def new
+    @post = Post.new
     render :new
   end
+
   def create
-    redirect_to new_post_path # redirects to GET "/posts/new"
+    @post = Post.new(post_params)
+
+    if params[:post][:image]
+      @post.image.attach(params[:post][:image])
+    end
+
+    if @post.save
+      redirect_to index_post_path, notice: '登録しました'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body, :image)
   end
 end
---
+++ app/models/post.rb
@@ -0,0 +1,8 @@
+class Post < ApplicationRecord
+  # Validation
+  validates :title, presence: true
+  validates :body, presence: true
+
+  # ActiveStorage
+  has_one_attached :image
+end
